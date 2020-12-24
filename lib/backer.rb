8fd95d9539b4f0594972e874a1e3f4b54b42dc43
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-24 04:26:25 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,12 +1,34 @@
 class Backer
   
-  attr_accessor :name
+  attr_accessor :name, :backed_projects
+  @@all = []
   
   def initialize(name)
   
+  @name = name
+  @backed_projects = []
+  @@all << self
   
+  end
   
-end
+  def back_project(project)
+  
+    @backed_projects << project
+  
+  end
+  
+  def backed_projects
+    
+    @backed_projects
+    
+  end
+  
+  def all
+    
+    @@all
+    
+  end
+  
 end
 
 
