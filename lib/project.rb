8aa247d11fb9d9e2a5782cf02791d110f5ce5bfe
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-24 04:28:57 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,5 +1,18 @@
 class Project
   
+  attr_accessor :name, :backers
   
+  def initialize(name)
+    
+    @name = name
+    @backers = []
+    
+  end
+  
+  def backers
+    
+    @backers
+    
+  end
   
 end
